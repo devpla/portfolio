1510e1f8772c57ae89d11dc014004eef2fe932c7
YeonHee <chaeyeonhee@kakao.com>
2022-02-19 21:38:48 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { GlobalStyle } from "./styles";
+
+const App = () => {
+  return (
+    <div>
+      <GlobalStyle />
+      Portfolio
+    </div>
+  );
+};
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+import normalize from "./normalize";
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  @font-face {
+    font-family: "Pretendard Variable";
+    font-weight: 45 920;
+    font-style: normal;
+    font-display: swap;
+    src: local("Pretendard Variable"),
+      url("./assets/PretendardVariable.woff2") format("woff2-variations");
+  }
+
+  * {
+    box-sizing: border-box;
+
+    font-family: "Pretendard Variable", -apple-system, BlinkMacSystemFont,
+      system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo",
+      "Noto Sans KR", "Malgun Gothic", sans-serif;
+    font-weight: 400;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    letter-spacing: -0.02rem;
+  }
+`;
+
+export default GlobalStyle;
